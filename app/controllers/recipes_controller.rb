74f5e50c94ce9cb2a7c13c72a56102bcabb9d75e
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-15 17:37:55 -0400

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,17 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times{@recipe.ingredients.build}
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    @recipe.save
+
+    redirect_to recipe_path(@recipe)
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
